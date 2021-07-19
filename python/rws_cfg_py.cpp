4fc895a2cad9142637b2733636a7bac71a500d86
Mike Danielczuk <michael.danielczuk@gmail.com>
2021-07-19 12:22:34 -0700

    fix namespaces

--- python/rws_cfg_py.cpp
+++ python/rws_cfg_py.cpp
@@ -8,21 +8,23 @@ namespace py = pybind11;
 
 namespace abb {
 namespace rws {
-namespace cfg {
-namespace moc {
 
-void init_cfg(py::module &m){
+void init_cfg(py::module &m) {
 
-    py::class_<Arm>(m, "Arm")
+    py::class_<cfg::moc::Arm>(m, "Arm")
         .def(py::init<const std::string&, double, double>(), "name"_a, "lower_joint_bound"_a, "upper_joint_bound"_a)
     ;
 
-    py::class_<Joint>(m, "Joint")
+    py::class_<cfg::moc::Joint>(m, "Joint")
         .def(py::init<const std::string&, int, int, const std::string&, const std::string&>(), "name"_a, "logical_axis"_a, "kinematic_axis_number"_a, "use_arm"_a, "use_transmission"_a)
     ;
     
-    py::class_<MechanicalUnit>(m, "MechanicalUnit")
+    py::class_<cfg::moc::MechanicalUnit>(m, "MechanicalUnit")
         .def(py::init<const std::string&, const std::string&, const std::vector<std::string>&>())
+    ;
+
+    py::class_<cfg::moc::Robot>(m, "Robot")
+        .def(py::init<const std::string&, const std::string&, const std::vector<std::string>&, const Pose&, const std::string&>())
     ;
 
 }
